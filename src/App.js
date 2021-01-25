adeb1725ba13954e7d89e81f0f3556f30b4160b8
trudy <truudy@kakao.com>
2021-01-25 21:59:45 +0900

    컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import {Route} from 'react-router-dom';
+import PostListPage from "./pages/PostListPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import WritePage from "./pages/WritePage";
+import PostPage from "./pages/PostPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return(
+      <Fragment>
+        <Route component={PostListPage} path={['/@:username', '/']} exact/>
+        <Route component={LoginPage} path={"/login"}/>
+        <Route component={RegisterPage} path={"/register"}/>
+        <Route component={WritePage} path={"/write"}/>
+        <Route component={PostPage} path={"/@:username/:postId"}/>
+      </Fragment>
   );
-}
+};
 
 export default App;
