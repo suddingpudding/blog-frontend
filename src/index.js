c8c6d661ea17adb56c5093caea6949645ed978bb
trudy <truudy@kakao.com>
2021-01-26 23:08:04 +0900

    auth 컴포넌트, redux, button style

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import AuthTemplate from "../components/auth/AuthTemplate";
+import AuthForm from "../components/auth/AuthForm";
 
 const LoginPage = () => {
-    return <div>로그인</div>;
+    return (
+        <AuthTemplate>
+            <AuthForm/>
+        </AuthTemplate>
+    );
 };
 
 export default LoginPage;
