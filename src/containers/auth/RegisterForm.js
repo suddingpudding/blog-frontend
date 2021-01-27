f3af696e8ce0050da9dc5a4b9f7207141e604907
trudy <truudy@kakao.com>
2021-01-27 21:30:28 +0900

    login, register Form 작성

---
+++ src/containers/auth/RegisterForm.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {changeField, initializeForm} from "../../modules/auth";
+import AuthForm from "../../components/auth/AuthForm";
+
+const LoginForm = () => {
+    const dispatch = useDispatch();
+    const {form} = useSelector(({auth}) => ({
+        form: auth.login
+    }));
+    // 인풋 변경 이벤트 핸들러
+    const onChange = e => {
+        const {value, name} = e.target;
+        dispatch(
+            changeField({
+                form: 'login',
+                key: name,
+                value
+            })
+        );
+    };
+
+    // 폼 등록 이벤트 핸들러
+    const onSubmit = e => {
+        e.preventDefault();
+        // 구현 예정
+    };
+
+    // 컴포넌트가 처음 랜더링될때 form을 초기화함
+    useEffect(() => {
+        dispatch(initializeForm('login'));
+    }, [dispatch]);
+
+    return (
+        <AuthForm
+            type={"login"}
+            form={form}
+            onChange={onChange}
+            onSubmit={onSubmit}
+        />
+    );
+};
+
+export default LoginForm;
