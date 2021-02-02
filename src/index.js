8e5cb09c1951678a27d14be3ac891c08bd1b279b
trudy <ssu9175@gmail.com>
2021-02-02 18:31:31 +0900

    스토어에 redux-saga 미들웨어 적용

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
-import rootReducer from "./modules";
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, {rootSaga} from "./modules";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
