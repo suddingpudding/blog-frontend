461b88739317917a4660475b10a208aea5b95e5c
trudy <ssu9175@gmail.com>
2021-02-03 17:05:12 +0900

    회원가입 성공 후 check를 호출하여 현재 사용자가 로그인 상태가 되었는지 확인

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -2,13 +2,15 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {changeField, initializeForm, register} from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
+import {check} from "../../modules/user";
 
 const RegisterForm = () => {
     const dispatch = useDispatch();
-    const {form} = useSelector(({auth}) => ({
+    const {form, auth, authError, user} = useSelector(({auth, user}) => ({
         form: auth.register,
         auth: auth.auth,
-        authError: auth.authError
+        authError: auth.authError,
+        user: user.user
     }));
 
     // 인풋 변경 이벤트 핸들러
@@ -41,16 +43,25 @@ const RegisterForm = () => {
 
     // 회원가입 성공/ 실패 처리
     useEffect(() => {
-        if (authError){
+        if (authError) {
             console.log('오류 발생');
             console.log(authError);
             return;
         }
-        if (auth){
+        if (auth) {
             console.log('회원가입 성공');
             console.log(auth);
+            dispatch(check());
         }
-    }, [auth, authError]);
+    }, [auth, authError, dispatch]);
+
+    useEffect(() => {
+            if (user) {
+                console.log('check API 성공');
+                console.log(user);
+            }
+        }, [user]
+    );
 
     return (
         <AuthForm
