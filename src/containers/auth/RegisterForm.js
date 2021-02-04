33f78b73731324356b3583f699497c17db50e5f1
trudy <ssu9175@gmail.com>
2021-02-04 17:58:32 +0900

    import 수정

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {changeField, initializeForm, register} from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
