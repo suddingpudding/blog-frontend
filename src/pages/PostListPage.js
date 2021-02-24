9f38169ac83491eab28060c4ac4965a9a0d3b680
trudy <ssu9175@gmail.com>
2021-02-24 19:27:31 +0900

    Header 컴포넌트 작성

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import Button from "../components/common/Button";
+import React, {Fragment} from 'react';
+import Header from "../components/common/Header";
 
 const PostListPage = () => {
     return (
-        <div>
-            <Button>버튼</Button>
-        </div>
+        <Fragment>
+            <Header/>
+            <div>안녕하세요.</div>
+        </Fragment>
     );
 };
 
