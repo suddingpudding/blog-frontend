ca28f52a966f746e123b4893d60cd6fa284d1026
trudy <ssu9175@gmail.com>
2021-03-02 18:33:26 +0900

    Header 컴포넌트를 HeaderContainer로 대체

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,10 +1,10 @@
 import React, {Fragment} from 'react';
-import Header from "../components/common/Header";
+import HeaderContainer from "../containers/common/HeaderContainer";
 
 const PostListPage = () => {
     return (
         <Fragment>
-            <Header/>
+            <HeaderContainer/>
             <div>안녕하세요.</div>
         </Fragment>
     );
