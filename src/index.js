621eed8d7ea6f1ad63cd0eb7ccb80e0f716ab44a
trudy <ssu9175@gmail.com>
2021-03-04 18:34:33 +0900

    사용자 정보를 불러오도록 처리하고 컴포넌트를 렌더링함.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {createStore, applyMiddleware} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, {rootSaga} from "./modules";
+import {tempSetuser, check} from "./modules/user";
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -16,7 +17,20 @@ const store = createStore(
     composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
+function loadUser() {
+    try {
+        const user = localStorage.getItem('user');
+        if (!user) return; //로그인 상태가 아니라면 아무것도 안 함.
+
+        store.dispatch(tempSetuser(user));
+        store.dispatch(check());
+    } catch (e) {
+        console.log('localStorage is not working');
+    }
+}
+
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
     <Provider store={store}>
