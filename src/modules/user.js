9248b183aab65a5a67c23d50ffc321046886ccf2
trudy <ssu9175@gmail.com>
2021-03-04 19:32:18 +0900

    LOGOUT이라는 액션을 만들고, 이 액션이 디스패치되었을때 API 호출 후 localStorage의 user 값을 지워줌. 추가로 리듀서에서는 스토어의 user 값을 null로 설정.

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,5 +1,5 @@
 import {createAction, handleActions} from 'redux-actions';
-import {takeLatest} from 'redux-saga/effects';
+import {call, takeLatest} from 'redux-saga/effects';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {createRequestActionTypes} from "../lib/createRequestSaga";
 
@@ -9,9 +9,11 @@ const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
     'user/CHECK'
 );
+const LOGOUT = 'user/LOGOUT';
 
-export const tempSetuser = createAction(TEMP_SET_USER, user => user);
+export const tempSetUser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
 
@@ -23,9 +25,19 @@ function checkFailureSaga() {
     }
 }
 
+function* logoutSaga() {
+    try {
+        yield call(authAPI.logout); // logout API 호출
+        localStorage.removeItem('user'); // localStorage에서 user를 제거
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 export function* userSaga() {
     yield takeLatest(CHECK, checkSaga);
     yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+    yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -48,6 +60,10 @@ export default handleActions(
             ...state,
             user: null,
             checkError: error
-        })
+        }),
+        [LOGOUT]: state => ({
+            ...state,
+            user: null
+        }),
     }, initialState
 );
