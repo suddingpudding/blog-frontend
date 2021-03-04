a59c2ac084428b4cdc62243badd5718360899ca3
trudy <ssu9175@gmail.com>
2021-03-04 18:43:25 +0900

    로그인 정보가 만료되었을 때를 대비하여 사용자 정보를 초기화하는 작업을 함.

--- src/modules/user.js
+++ src/modules/user.js
@@ -14,8 +14,18 @@ export const tempSetuser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
+
+function checkFailureSaga() {
+    try {
+        localStorage.removeItem('user'); // localStorage에서 user를 제거
+    } catch (e) {
+        console.log('localStorage is not working');
+    }
+}
+
 export function* userSaga() {
     yield takeLatest(CHECK, checkSaga);
+    yield takeLatest(CHECK_FAILURE, checkFailureSaga);
 }
 
 const initialState = {
