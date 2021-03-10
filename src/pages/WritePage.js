10d961c356a2c8c981b36f6c9d40d960ceb50f77
trudy <ssu9175@gmail.com>
2021-03-10 19:10:30 +0900

    TagBox 컴포넌트 렌더링

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Editor from "../components/write/Editor";
 import Responsive from "../components/common/Responsive";
+import TagBox from "../components/write/TagBox";
 
 const WritePage = () => {
     return (
         <Responsive>
             <Editor/>
+            <TagBox/>
         </Responsive>
     );
 };
