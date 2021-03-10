fab7b512613cb3fe61ab73ac6109495f5305482a
trudy <ssu9175@gmail.com>
2021-03-10 16:22:20 +0900

    Editor 컴포넌트를 WritePage에 렌더링 함.

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Editor from "../components/write/Editor";
+import Responsive from "../components/common/Responsive";
 
 const WritePage = () => {
-    return <div>글쓰기</div>;
+    return (
+        <Responsive>
+            <Editor/>
+        </Responsive>
+    );
 };
 
 export default WritePage;
