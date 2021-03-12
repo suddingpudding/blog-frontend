d2ab66c20382a0223731573696716f3dc556d82d
trudy <ssu9175@gmail.com>
2021-03-12 16:52:28 +0900

    WriteActionButtons 컴포넌트를 렌더링함.

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Editor from "../components/write/Editor";
 import Responsive from "../components/common/Responsive";
 import TagBox from "../components/write/TagBox";
+import WriteActionButtons from "../components/write/WriteActionButtons";
 
 const WritePage = () => {
     return (
         <Responsive>
             <Editor/>
             <TagBox/>
+            <WriteActionButtons/>
         </Responsive>
     );
 };
