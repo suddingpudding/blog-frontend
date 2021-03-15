709c48952b93c796a8fa864ad602ad1bb916310b
trudy <ssu9175@gmail.com>
2021-03-15 17:26:58 +0900

    리덕스 개발자 도구를 확인하여 에디터에 입력한 값이 스토어에도 그대로 반영되게함.

--- src/components/write/Editor.js
+++ src/components/write/Editor.js
@@ -34,7 +34,7 @@ const QuillWrapper = styled.div`
   }
 `;
 
-const Editor = () => {
+const Editor = ({title, body, onChangeField}) => {
     const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
     const quillInstance = useRef(null); // Quill 인스턴스를 설정
 
@@ -53,11 +53,27 @@ const Editor = () => {
                 ]
             }
         });
-    }, []);
+
+        // quill에 text-change 이벤트 핸들러 등록
+        // 참고: https://quilljs.com/docs/api/#events
+        const quill = quillInstance.current;
+        quill.on('text-change', (delta, oldDelta, source) => {
+            if (source === 'user') {
+                onChangeField({key: 'body', value: quill.root.innerHTML});
+            }
+        });
+    }, [onChangeField]);
+    const onChangeTitle = e => {
+        onChangeField({key: 'title', value: e.target.value});
+    };
 
     return (
         <EditorBlock>
-            <TitleInput placeholder={"제목을 입력하세요"}/>
+            <TitleInput
+                placeholder={"제목을 입력하세요"}
+                onChange={onChangeField}
+                value={title}
+            />
             <QuillWrapper>
                 <div ref={quillElement}/>
             </QuillWrapper>
