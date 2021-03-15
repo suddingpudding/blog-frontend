fb1954f47e33f0f5480d968a5742b27584dc5594
trudy <ssu9175@gmail.com>
2021-03-15 17:35:04 +0900

    수정

--- src/components/write/WriteActionButtons.js
+++ src/components/write/WriteActionButtons.js
@@ -5,6 +5,7 @@ import Button from "../common/Button";
 const WriteActionButtonsBlock = styled.div`
   margin-top: 1rem;
   margin-bottom: 3rem;
+
   button + button {
     margin-left: 0.5rem;
   }
@@ -13,20 +14,21 @@ const WriteActionButtonsBlock = styled.div`
 /* TagBox에서 사용하는 버튼과 일치하는 높이로 설정한 후 서로 간의 여백 지정 */
 const StyledButton = styled(Button)`
   height: 2.125rem;
+
   & + & {
     margin-left: 0.5rem;
   }
 `;
 
 const WriteActionButtons = ({onCancel, onPublish}) => {
-  return(
-     <WriteActionButtonsBlock>
-         <StyledButton cyan onClick={{onPublish}}>
-             포스트 등록
-         </StyledButton>
-         <StyledButton onClick={onCancel}>취소</StyledButton>
-     </WriteActionButtonsBlock>
-  );
+    return (
+        <WriteActionButtonsBlock>
+            <StyledButton cyan onClick={{onPublish}}>
+                포스트 등록
+            </StyledButton>
+            <StyledButton onClick={onCancel}>취소</StyledButton>
+        </WriteActionButtonsBlock>
+    );
 };
 
 export default WriteActionButtons;
