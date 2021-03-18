202f5a86a082f135ae2229bc5246876c700aa296
trudy <ssu9175@gmail.com>
2021-03-18 19:05:16 +0900

    props를 PostViewer 컴포넌트에서 사용

--- src/components/post/PostViewer.js
+++ src/components/post/PostViewer.js
@@ -50,24 +50,38 @@ const PostContent = styled.div`
   color: ${palette.gray[8]};
 `;
 
-const PostViewer = () => {
+const PostViewer = ({post, error, loading}) => {
+    // 에러 발생 시
+    if (error) {
+        if (error.response && error.response.status === 404) {
+            return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
+        }
+        return <PostViewerBlock>오류 발생!</PostViewerBlock>;
+    }
+
+    // 로딩 중이거나 아직 포스트 데이터가 없을 때
+    if (loading || !post) {
+        return null;
+    }
+
+    const {title, body, user, publishedDate, tags} = post;
     return (
         <PostViewerBlock>
             <PostHead>
-                <h1>제목</h1>
+                <h1>{title}</h1>
                 <SubInfo>
                     <span>
-                        <b>tester</b>
+                        <b>{user.username}</b>
                     </span>
-                    <span>{new Date().toLocaleDateString()}</span>
+                    <span>{new Date(publishedDate).toLocaleDateString()}</span>
                 </SubInfo>
                 <Tags>
-                    <div className={"tag"}>#태그1</div>
-                    <div className={"tag"}>#태그2</div>
-                    <div className={"tag"}>#태그3</div>
+                    {tags.map(tag => (
+                        <div className={tag}>#{tag}</div>
+                    ))}
                 </Tags>
             </PostHead>
-            <PostContent dangerouslySetInnerHTML={{__html: '<p>HTML <b>내용</b>입니다.</p>'}}/>
+            <PostContent dangerouslySetInnerHTML={{__html: body}}/>
         </PostViewerBlock>
     );
 };
