2c8d708a42055b188181859ded92487064cb6665
trudy <ssu9175@gmail.com>
2021-03-18 16:01:50 +0900

    오타 수정

--- src/containers/write/WriteActionButtonsContainer.js
+++ src/containers/write/WriteActionButtonsContainer.js
@@ -34,7 +34,7 @@ const WriteActionButtonsContainer = ({history}) => {
     useEffect(() => {
         if (post) {
             const {_id, user} = post;
-            history.push('/@${user.username}/${_id}');
+            history.push(`/@${user.username}/${_id}`);
         }
         if (postError) {
             console.log(postError);
