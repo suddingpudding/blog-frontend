589d3bf72e0b840f29cd35acdf314d4fd5e185a1
trudy <ssu9175@gmail.com>
2021-03-18 16:05:06 +0900

    WriteActionButtons 컴포넌트를 WriteActionButtonsContainer로 대체

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Responsive from "../components/common/Responsive";
-import WriteActionButtons from "../components/write/WriteActionButtons";
 import EditorContainer from "../components/write/EditorContainer";
 import TagBoxContainer from "../components/write/TagBoxContainer";
+import WriteActionButtonsContainer from "../containers/write/WriteActionButtonsContainer";
 
 const WritePage = () => {
     return (
         <Responsive>
             <EditorContainer/>
             <TagBoxContainer/>
-            <WriteActionButtons/>
+            <WriteActionButtonsContainer/>
         </Responsive>
     );
 };
