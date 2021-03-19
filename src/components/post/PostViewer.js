a97fd371790458d799f8605b954a79fd9412a092
trudy <ssu9175@gmail.com>
2021-03-19 16:47:02 +0900

    중복코드로 분리하여 만든 컴포넌트를 사용하여 코드를 간결하게 수정함.

--- src/components/post/PostViewer.js
+++ src/components/post/PostViewer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from "styled-components";
 import palette from "../../lib/styles/palette";
 import Responsive from "../common/Responsive";
+import SubInfo from "../common/SubInfo";
+import Tags from "../common/Tags";
 
 const PostViewerBlock = styled(Responsive)`
   margin-top: 4rem;
@@ -18,33 +20,7 @@ const PostHead = styled.div`
     margin: 0;
   }
 `;
-const SubInfo = styled.div`
-  margin-top: 1rem;
-  color: ${palette.gray[6]};
 
-  /* span 사이에 가운뎃점 문자 보여 주기 */
-
-  span + span:before {
-    color: ${palette.gray[5]};
-    padding-left: 0.25rem;
-    padding-right: 0.25rem;
-    content: '\\B7'; /* 가운뎃점 문자 */
-  }
-`;
-const Tags = styled.div`
-  margin-top: 0.5rem;
-
-  .tag {
-    display: inline-block;
-    color: ${palette.cyan[7]};
-    text-decoration: none;
-    margin-right: 0.5rem;
-
-    &:hover {
-      color: ${palette.cyan[6]};
-    }
-  }
-`;
 const PostContent = styled.div`
   font-size: 1.3125rem;
   color: ${palette.gray[8]};
@@ -69,19 +45,14 @@ const PostViewer = ({post, error, loading}) => {
         <PostViewerBlock>
             <PostHead>
                 <h1>{title}</h1>
-                <SubInfo>
-                    <span>
-                        <b>{user.username}</b>
-                    </span>
-                    <span>{new Date(publishedDate).toLocaleDateString()}</span>
-                </SubInfo>
-                <Tags>
-                    {tags.map(tag => (
-                        <div className={"tag"}>#{tag}</div>
-                    ))}
-                </Tags>
+                <SubInfo
+                    username={user.username}
+                    publishedDate={publishedDate}
+                    hasMarginTop
+                />
+                <Tags tags={tags}/>
             </PostHead>
-            <PostContent dangerouslySetInnerHTML={{ __html: body}}/>
+            <PostContent dangerouslySetInnerHTML={{__html: body}}/>
         </PostViewerBlock>
     );
 };
