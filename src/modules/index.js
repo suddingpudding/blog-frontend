bde63716e0a9f1264f4bd22d8d684ea861b8e2c7
trudy <ssu9175@gmail.com>
2021-03-19 18:19:48 +0900

    루트 리듀서와 루트 사가에 방금만든 리듀서와 사가를 등록함.

--- src/modules/index.js
+++ src/modules/index.js
@@ -5,17 +5,20 @@ import loading from "./loading";
 import user, {userSaga} from "./user";
 import write, {writeSaga} from "./write";
 import post, {postSaga} from "./post";
+import posts, {postsSaga} from "./posts";
 
 const rootReducer = combineReducers({
     auth,
     loading,
     user,
     write,
-    post
+    post,
+    posts
+
 });
 
 export function* rootSaga() {
-    yield all([authSaga(), userSaga(), writeSaga(), postSaga()]);
+    yield all([authSaga(), userSaga(), writeSaga(), postSaga(), postsSaga()]);
 }
 
 export default rootReducer;
