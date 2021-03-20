2856bcbf1bc17475b660f66edd1155248049e07a
trudy <ssu9175@gmail.com>
2021-03-20 14:58:15 +0900

    PostList에서 받아온 props에 따라 결과물을 보여줌.

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import {Link} from "react-router-dom";
 import Responsive from "../common/Responsive";
 import palette from "../../lib/styles/palette";
 import Button from "../common/Button";
@@ -44,45 +45,49 @@ const PostItemBlock = styled.div`
   }
 `;
 
-const PostItem = () => {
+const PostItem = ({post}) => {
+    const {publishedDate, user, tags, title, body, _id} = post;
     return (
         <PostItemBlock>
-            <h2>제목</h2>
-            <SubInfo username={"username"} publishedDate={new Date()}/>
-            <Tags tags={['태그1', '태그2', '태그3']}/>
-            <p>포스트 내용의 일부분..</p>
+            <h2>
+                <Link to={`/@${user.username}/${_id}`}>{title}</Link>
+            </h2>
+            <SubInfo
+                username={user.username}
+                publishedDate={new Date(publishedDate)}/>
+            <Tags tags={tags}/>
+            <p>{body}</p>
         </PostItemBlock>
     );
 };
 
-const PostList = () => {
+const PostList = ({posts, loading, error, showWriteButton}) => {
+    // 에러 발생 시
+    if (error) {
+        return <PostListBlock>에러가 발생했습니다.</PostListBlock>;
+    }
     return (
         <PostListBlock>
             <WritePostButtonWrapper>
-                <Button cyan to="/write">
-                    새 글 작성하기
-                </Button>
+                {showWriteButton && (
+                    <Button cyan to="/write">
+                        새 글 작성하기
+                    </Button>
+                )}
             </WritePostButtonWrapper>
-            <div>
-                <PostItem/>
-                <PostItem/>
-                <PostItem/>
-            </div>
+            {/* 로딩 중이 아니고, 포스트 배열리 존재할 때만 보여 줌 */}
+            {!loading && posts && (
+                <div>
+                    {posts.map(post => (
+                        <PostItem post={post} key={post._id}/>
+                    ))}
+                </div>
+            )}
         </PostListBlock>
     );
 };
 
 export default PostList;
-
-
-
-
-
-
-
-
-
-
 
 
 
