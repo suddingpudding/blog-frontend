e2a227be9eccfe5419ae86fc7bf56cfc429f52a5
trudy <ssu9175@gmail.com>
2021-03-23 16:13:43 +0900

    이 컴포넌트는 주소에 있는 쿼리 파라미터를 추출하여 우리가 만들었던 listPosts API를 호출해 줌.

--- src/containers/posts/PostListContainer.js
+++ src/containers/posts/PostListContainer.js
@@ -1,36 +1,35 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from 'react';
 import qs from 'qs';
-import {withRouter} from 'react-router-dom';
-import {useDispatch, useSelector} from "react-redux";
-import {listPosts} from "../../modules/posts";
-import PostList from "../../components/posts/PostList";
+import { withRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import PostList from '../../components/posts/PostList';
+import { listPosts } from '../../modules/posts';
 
-const PostListContainer = ({location, match}) => {
-  const dispatch = useDispatch();
-  const {posts, error, loading, user} = useSelector(
-      ({ posts, loading, user}) => ({
-        posts: posts.posts,
-        error: posts.error,
-        loading: loading['posts/LIST_POSTS'],
-        user: user.user
-      })
-  );
-  useEffect(() => {
-    const {username} = match.parse;
-    const {tag, page} = qs.parse(location.search, {
-      ignoreQueryPrefix: true
-    });
-    dispatch(listPosts({tag, username, page}));
-  }, [dispatch, location.search]);
+const PostListContainer = ({ location }) => {
+    const dispatch = useDispatch();
+    const { posts, error, loading, user } = useSelector(
+        ({ posts, loading, user }) => ({
+            posts: posts.posts,
+            error: posts.error,
+            loading: loading['posts/LIST_POSTS'],
+            user: user.user,
+        }),
+    );
+    useEffect(() => {
+        const { tag, username, page } = qs.parse(location.search, {
+            ignoreQueryPrefix: true,
+        });
+        dispatch(listPosts({ tag, username, page }));
+    }, [dispatch, location.search]);
 
-  return(
-      <PostList
-        loading={loading}
-        error={error}
-        posts={posts}
-        showWriteButton={user}
-      />
-  );
+    return (
+        <PostList
+            loading={loading}
+            error={error}
+            posts={posts}
+            showWriteButton={user}
+        />
+    );
 };
 
 export default withRouter(PostListContainer);
