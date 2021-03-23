be1a5254c8629aac1d225ecaee1aa6a718b5a57b
trudy <ssu9175@gmail.com>
2021-03-23 17:11:22 +0900

    이제 리덕스 스토어 안에 마지막 페이지 번호를 lastPage라는 값으로 담아 둘 수 있음.

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -22,14 +22,16 @@ export function* postsSaga() {
 
 const initialState = {
     posts: null,
-    error: null
+    error: null,
+    lastPage: 1
 };
 
 const posts = handleActions(
     {
-        [LIST_POSTS_SUCCESS]: (state, {payload: posts}) => ({
+        [LIST_POSTS_SUCCESS]: (state, {payload: posts, meta: response}) => ({
             ...state,
-            posts
+            posts,
+            lastPage: parseInt(response.headers['last-page'], 10) // 문자열을 숫자로 변환
         }),
         [LIST_POSTS_FAILURE]: (state, {payload: error}) => ({
             ...state,
