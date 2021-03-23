c81579f0cf88a398728c9f2308664e3c42169a59
trudy <ssu9175@gmail.com>
2021-03-23 19:00:27 +0900

    Button 컴포넌트에 비활성화된 스타일을 설정해줌. 비활성화 스타일은 :disabled CSS 셀렉터를 사용하여 적용할 수 있음.

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -13,6 +13,7 @@ const buttonStyle = css`
   outline: none;
   cursor: pointer;
   background: ${palette.gray[8]};
+
   &:hover {
     background: ${palette.gray[6]};
   }
@@ -25,15 +26,20 @@ const buttonStyle = css`
             width: 100%;
             font-size: 1.125rem;
           `}
-
   ${props =>
           props.cyan &&
           css`
             background: ${palette.cyan[5]};
+
             &:hover {
               background: ${palette.cyan[4]};
             }
           `}
+  &:disabled {
+    background: ${palette.gray[3]};
+    color: ${palette.gray[5]};
+    cursor: not-allowed;
+  }
 `;
 
 const StyleButton = styled.button`
