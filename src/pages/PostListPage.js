a51fe9ec3062aa2a58a055e1ad8ca112d4a404fc
trudy <ssu9175@gmail.com>
2021-03-23 19:23:44 +0900

    PaginationContainer 컨테이너 컴포넌트를 PostListPage에서 렌더링해줌. 페이지네이션 기능 구현 끝.

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react';
 import HeaderContainer from "../containers/common/HeaderContainer";
 import PostListContainer from "../containers/posts/PostListContainer";
+import PaginationContainer from "../containers/posts/PaginationContainer";
 
 const PostListPage = () => {
     return (
         <Fragment>
             <HeaderContainer/>
             <PostListContainer/>
+            <PaginationContainer/>
         </Fragment>
     );
 };
