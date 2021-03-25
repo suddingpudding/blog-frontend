df16bdc72644fa7c0737dbc3fe1adc30b0f4b5b1
trudy <ssu9175@gmail.com>
2021-03-25 18:03:42 +0900

    수정버튼을 클릭하면 글쓰기 페이지로 이동하고, 현재 보고 있는 포스트가 나타나게 해볼예정. 우선 write 리덕스 모듈에 SET_ORIGINAL_POST라는 액션을 만듬. 이 액션은 현재 보고 있는 포스트 정보를 write 모듈에서 관리하는 상태에 넣음.

--- src/modules/write.js
+++ src/modules/write.js
@@ -10,6 +10,7 @@ const [
     WRITE_POST_SUCCESS,
     WRITE_POST_FAILURE
 ] = createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
+const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST';
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({key, value}) => ({
@@ -21,6 +22,7 @@ export const writePost = createAction(WRITE_POST, ({title, body, tags}) => ({
     body,
     tags
 }));
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post);
 
 // 사가 생성
 const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
@@ -34,7 +36,8 @@ const initialState = {
     body: '',
     tags: [],
     post: null,
-    postError: null
+    postError: null,
+    originalPostId: null
 };
 
 const write = handleActions({
@@ -58,6 +61,13 @@ const write = handleActions({
     [WRITE_POST_FAILURE]: (state, {payload: postError}) => ({
         ...state,
         postError
+    }),
+    [SET_ORIGINAL_POST]: (state, {payload: post}) => ({
+        ...state,
+        title: post.title,
+        body: post.body,
+        tags: post.tags,
+        originalPostId: post._id
     })
 }, initialState);
 
