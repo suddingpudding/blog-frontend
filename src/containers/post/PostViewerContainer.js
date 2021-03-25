487b8bfd19544ee39f99aec0d759c7d5c25ec839
trudy <ssu9175@gmail.com>
2021-03-25 18:11:31 +0900

    기존에는 PostActionButtons가 무조건 나타났는데, 현재 사용자가 보고 있는 포스트가 자신의 포스트일 때만 PostActionButtons가 나타나게 설정함. 그리고 onEdit 함수를 구현하여 PostActionButtons에 전달해줌.

--- src/containers/post/PostViewerContainer.js
+++ src/containers/post/PostViewerContainer.js
@@ -4,15 +4,17 @@ import {withRouter} from 'react-router-dom'
 import {readPost, unloadPost} from "../../modules/post";
 import PostViewer from "../../components/post/PostViewer";
 import PostActionButtons from "../../components/posts/PostActionButtons";
+import {setOriginalPost} from "../../modules/write";
 
-const PostViewerContainer = ({match}) => {
+const PostViewerContainer = ({match, history}) => {
     // 처음 마운트될 때 포스트 읽기 API 요청
     const {postId} = match.params;
     const dispatch = useDispatch();
-    const {post, error, loading} = useSelector(({post, loading}) => ({
+    const {post, error, loading, user} = useSelector(({post, loading, user}) => ({
         post: post.post,
         error: post.error,
-        loading: loading['post/READ_POST']
+        loading: loading['post/READ_POST'],
+        user: user.user
     }));
 
     useEffect(() => {
@@ -23,12 +25,18 @@ const PostViewerContainer = ({match}) => {
         };
     }, [dispatch, postId]);
 
+    const onEdit = () => {
+        dispatch(setOriginalPost(post));
+        history.push('/write');
+    };
+
+    const ownPost = (user && user._id) === (post && post.user._id);
     return (
         <PostViewer
             post={post}
             loading={loading}
             error={error}
-            actionButtons={<PostActionButtons/>}
+            actionButtons={ownPost && <PostActionButtons onEdit={onEdit}/>}
         />
     );
 };
