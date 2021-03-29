c1b6bcef3bc0d28e06f831f338c1e29150b0720a
trudy <ssu9175@gmail.com>
2021-03-29 18:56:20 +0900

    PostViewer에서 onRemove 함수를 만들어 removePost를 호출하도록 구현함. removePost의 경우에는 API를 요청한 후 따로 보여 주어야 할 결과가 없으니 리덕스 액션과 사가를 만드는 작업을 생략하고 바로 API를 사용함.

--- src/containers/post/PostViewerContainer.js
+++ src/containers/post/PostViewerContainer.js
@@ -5,6 +5,7 @@ import {readPost, unloadPost} from "../../modules/post";
 import PostViewer from "../../components/post/PostViewer";
 import PostActionButtons from "../../components/post/PostActionButtons";
 import {setOriginalPost} from "../../modules/write";
+import {removePost} from "../../lib/api/posts";
 
 const PostViewerContainer = ({match, history}) => {
     // 처음 마운트될 때 포스트 읽기 API 요청
@@ -28,6 +29,15 @@ const PostViewerContainer = ({match, history}) => {
     const onEdit = () => {
         dispatch(setOriginalPost(post));
         history.push('/write');
+    };
+
+    const onRemove = async () => {
+        try {
+            await removePost(postId);
+            history.push('/'); // 홈으로 이동
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     const ownPost = (user && user._id) === (post && post.user._id);
