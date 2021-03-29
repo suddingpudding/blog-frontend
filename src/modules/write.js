5ed01b8ecb99289fc4b9673bb31363183d7ed114
trudy <ssu9175@gmail.com>
2021-03-29 17:14:15 +0900

    write 리덕스 모듈에서 UPDATE_POST 액션과 updatePostSaga를 만듬.

--- src/modules/write.js
+++ src/modules/write.js
@@ -11,6 +11,11 @@ const [
     WRITE_POST_FAILURE
 ] = createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
 const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST';
+const [
+    UPDATE_POST,
+    UPDATE_POST_SUCCESS,
+    UPDATE_POST_FAILURE
+] = createRequestActionTypes('write/UPDATE_POST'); // 포스트 수정
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({key, value}) => ({
@@ -23,12 +28,23 @@ export const writePost = createAction(WRITE_POST, ({title, body, tags}) => ({
     tags
 }));
 export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post);
+export const updatePost = createAction(
+    UPDATE_POST,
+    ({id, title, body, tags}) => ({
+        id,
+        title,
+        body,
+        tags
+    })
+);
 
 // 사가 생성
 const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
+const updatePostSaga = createRequestSaga(UPDATE_POST, postsAPI.updatePost);
 
 export function* writeSaga() {
     yield takeLatest(WRITE_POST, writePostSaga);
+    yield takeLatest(UPDATE_POST, updatePostSaga);
 }
 
 const initialState = {
@@ -68,6 +84,14 @@ const write = handleActions({
         body: post.body,
         tags: post.tags,
         originalPostId: post._id
+    }),
+    [UPDATE_POST_SUCCESS]: (state, {payload: post}) => ({
+        ...state,
+        post
+    }),
+    [UPDATE_POST_FAILURE]: (state, {payload: postError}) => ({
+        ...state,
+        postError
     })
 }, initialState);
 
